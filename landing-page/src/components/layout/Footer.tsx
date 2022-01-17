50d7ca892b9e8b4a7c2f24b5c1e6bfa37d7b5433
Trenten <trenten.grede@gmail.com>
2022-01-17 10:39:25 -0600

    Added logo to footer

--- landing-page/src/components/layout/Footer.tsx
+++ landing-page/src/components/layout/Footer.tsx
@@ -1,12 +1,18 @@
 import styled from 'styled-components'
 import SocialIcons from '../buttons/SocialIcons'
+import logo from '../../assets/horizontal_logo_orange&white.svg'
 
 function Footer() {
   return (
     <div>
+      <Img src={logo} alt={"Y-Foundry Logo"} />
       <SocialIcons />
     </div>
   )
 }
+
+const Img = styled.img`
+  width:50px;
+`
 
 export default Footer
