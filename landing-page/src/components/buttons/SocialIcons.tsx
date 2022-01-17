71fb73c55f569adb57b0ca6bad16ccbcece034d4
Trenten <trenten.grede@gmail.com>
2022-01-17 14:38:54 -0600

    imported all social logos and styled-components

--- landing-page/src/components/buttons/SocialIcons.tsx
+++ landing-page/src/components/buttons/SocialIcons.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import twitter from '../../assets/twitter.svg'
+import discord from '../../assets/discord.svg'
+import telegram from '../../assets/telegram.svg'
 
 function SocialIcons() {
   return (
-    <div>
-      
-    </div>
+    <ul>
+      <li><img src={twitter} alt={"twitter"} /></li>
+      <li><img src={discord} alt={"discord"} /></li>
+      <li><img src={telegram} alt={"telegram"} /></li>
+    </ul>
   )
 }
 
