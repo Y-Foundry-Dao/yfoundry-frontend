29ac46b8e78b4e52f9adb3e316d50d7f867978d7
Trenten <trenten.grede@gmail.com>
2022-01-23 13:20:03 -0600

    Created shell of RadialGradient Component

---
+++ landing-page/src/components/background/RadialGradient.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+function RadialGradient() {
+  return (
+      <Radial>
+
+      </Radial>
+    )
+}
+
+const Radial = styled.div`
+  background: radial-gradient(50% 50% at 50% 50%, rgba(65, 97, 223, 0.2) 0%, rgba(66, 92, 222, 0) 100%);
+`
+
+export default RadialGradient;
