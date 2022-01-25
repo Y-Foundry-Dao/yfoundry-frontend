0fb45179076be6dece51ea7615475fed441357d0
Trenten <trenten.grede@gmail.com>
2022-01-25 14:08:12 -0600

    Created start of investorCarousel with swiper.js

--- landing-page/src/components/layout/InvestorCarousel.tsx
+++ landing-page/src/components/layout/InvestorCarousel.tsx
@@ -7,49 +7,48 @@ import hyperion from '../../assets/HyperionLogo.png';
 import lambda from '../../assets/LambdaLogo.png';
 import thorstarter from '../../assets/Thorstarter.svg';
 import northRock from '../../assets/NorthRockDigital.png';
+// Core modules imports are same as usual
+
+// Direct React component imports
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay, FreeMode } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
 
 
 function InvestorCarousel() {
   return (
-    <Carousel>
-      <LogoHolder1>
-        <Logo0xVentures src={xVentures} alt={"0xVentures Logo"} />
-      </LogoHolder1>
-
-      <LogoHolder>
-        <Logo src={hyperion} alt={"Hyperion Logo"} />
-      </LogoHolder>
-      
-      <LogoHolder>
-        <Logo src={apollo} alt={"Apollo"} /> 
-      </LogoHolder>
-      
-      <LogoHolder>
-        <Logo src={cetaceanCapital} alt={"Cetacean Capital Logo"} />
-      </LogoHolder>
-
-      <LogoHolder>
-        <Logo src={deepVentures} alt={"Deep Ventures logo"} />
-      </LogoHolder>
-     
-      <LogoHolder>
-        <Logo src={lambda} alt={"Lambda Logo"} />
-      </LogoHolder>
-      
-      <LogoHolder>
-        <Logo src={thorstarter} alt={"Thorstarter Logo"} />
-      </LogoHolder>
-      
-      <LogoHolder>
-        <Logo src={northRock} alt={"North Rock Digital Logo"} />
-      </LogoHolder>
-    </Carousel>
+    <StyledSwiper
+    modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, FreeMode]}
+    spaceBetween={0}
+    slidesPerView={4}
+    navigation
+    // autoplay={{delay:0, pauseOnMouseEnter:true, disableOnInteraction:false}}
+    freeMode
+    loop={true}
+    loopPreventsSlide={true}
+    speed={12000}
+    onSwiper={(swiper) => console.log(swiper)}
+    onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide><Logo0xVentures src={xVentures} alt={"0xVentures Logo"} /></SwiperSlide>
+      <SwiperSlide><Logo src={hyperion} alt={"Hyperion Logo"} /></SwiperSlide>
+      <SwiperSlide><Logo src={apollo} alt={"Apollo"} /></SwiperSlide>
+      <SwiperSlide><Logo src={cetaceanCapital} alt={"Cetacean Capital Logo"} /></SwiperSlide>
+      <SwiperSlide><Logo src={deepVentures} alt={"Deep Ventures logo"} /></SwiperSlide>
+      <SwiperSlide><Logo src={lambda} alt={"Lambda Logo"} /></SwiperSlide>
+      <SwiperSlide><Logo src={thorstarter} alt={"Thorstarter Logo"} /></SwiperSlide>
+      <SwiperSlide><Logo src={northRock} alt={"North Rock Digital Logo"} /></SwiperSlide>
+    </StyledSwiper>
   )
 }
 
-const Carousel = styled.div`
-  border:1px solid white;
-  padding: 2%;
+const StyledSwiper = styled(Swiper)`
+  padding-top: 2%;
   border: 1px ${props => `${props.theme.colors.orange}`};
   border-style: solid none;
   display: flex;
@@ -63,10 +62,9 @@ const Carousel = styled.div`
   }
 `
 
-const LogoHolder = styled.div`
+const StyledSwiperSlide = styled.div`
   display: flex;
   align-items: center;
-  /* border:1px solid white; */
   @media(max-width:756px) {
     margin: 5px 0 5px;
   }
