519eeac3248de83ac9e979137fff6e1adf1e1ce0
Trenten <trenten.grede@gmail.com>
2022-01-26 13:03:46 -0600

    Fixed sizing issues

--- landing-page/src/components/layout/InvestorCarousel.tsx
+++ landing-page/src/components/layout/InvestorCarousel.tsx
@@ -1,4 +1,4 @@
-import styled, {keyframes} from 'styled-components';
+import styled from 'styled-components';
 import xVentures from '../../assets/0xVenturesWhite.svg';
 import apollo from '../../assets/Apollo.png';
 import cetaceanCapital from '../../assets/CetaceanCapital.png';
@@ -10,49 +10,37 @@ import northRock from '../../assets/NorthRockDigital.png';
 // Core modules imports are same as usual
 
 // Direct React component imports
-import { Navigation, A11y, Autoplay, FreeMode } from 'swiper';
+import { A11y, Autoplay, FreeMode } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-
-
-
 
 function InvestorCarousel() {
   return (
     <StyledSwiper
-    slidesPerView={1}
-    spaceBetween={20}
+    slidesPerView={2}
     loop={true}
     centeredSlides={true}
     grabCursor={true}
     loopFillGroupWithBlank={true}
-    navigation={true}
     className="mySwiper"
-    modules={[Navigation, A11y, Autoplay, FreeMode]}
+    modules={[A11y, Autoplay, FreeMode]}
     autoplay={{delay:0, pauseOnMouseEnter:true, disableOnInteraction:false}}
     freeMode
     loopPreventsSlide={true}
     speed={10000}
     breakpoints={{
-      425:{
-        slidesPerView:2
-      },
       756:{
-        slidesPerView:3
+        slidesPerView:4
       }
     }}
     >
-      
       <SwiperSlide><Logo src={hyperion} alt={"Hyperion Logo"} /></SwiperSlide>
       <SwiperSlide><Logo src={apollo} alt={"Apollo"} /></SwiperSlide>
-      <SwiperSlide><Logo src={deepVentures} alt={"Deep Ventures logo"} /></SwiperSlide>
+      <SwiperSlide><LogoDeepVentures src={deepVentures} alt={"Deep Ventures logo"} /></SwiperSlide>
       <SwiperSlide><Logo src={cetaceanCapital} alt={"Cetacean Capital Logo"} /></SwiperSlide>
       <SwiperSlide><Logo src={lambda} alt={"Lambda Logo"} /></SwiperSlide>
       <SwiperSlide><Logo src={thorstarter} alt={"Thorstarter Logo"} /></SwiperSlide>
-      <SwiperSlide><Logo0xVentures src={xVentures} alt={"0xVentures Logo"} /></SwiperSlide>
+      <SwiperSlide><Logo src={xVentures} alt={"0xVentures Logo"} /></SwiperSlide>
       <SwiperSlide><Logo src={northRock} alt={"North Rock Digital Logo"} /></SwiperSlide>
     </StyledSwiper>
   )
@@ -89,22 +77,9 @@ const Logo = styled.img`
   }
 `
 
-const Logo0xVentures = styled(Logo)`
-  /* height:80px; */
-  /* height:60px;
-  padding-top:0px;
-  border:1px solid white;
-  padding-bottom:10px;
-
-  @media(min-width:425px){
-    height:68px;
-  }
-  @media(min-width:756px){
-    height:80px;
-  }
-  @media(min-width:1024px){
-    height:90px;
-  } */
+//Sets the padding on the Deep Ventures logo to fix its individual alignment
+const LogoDeepVentures = styled(Logo)`
+  padding-left:50px;
 `
 
 export default InvestorCarousel
