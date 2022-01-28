78a9cb0d95f6e149ce44d31962361d24effc553c
Trenten <trenten.grede@gmail.com>
2022-01-27 23:08:27 -0600

    Moved ScrollTopArrow component into components folder

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -7,7 +7,7 @@ import FooterBar from './components/sections/footer/FooterBar';
 import Community from './components/sections/vision/Vision';
 import Blog from './components/sections/blog/Blog';
 import BackgroundComposition from './components/background/BackgroundComposition';
-import ScrollButton from './components/buttons/ScrollTopArrow';
+import ScrollButton from './components/ScrollTopArrow';
 
 function App() {
   return (
