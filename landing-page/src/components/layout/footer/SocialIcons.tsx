07fc5c22fb34b12de80dcefa6c1a2962b9b04705
Trenten <trenten.grede@gmail.com>
2022-01-27 22:53:18 -0600

    Moved footer components to footer folder

---
+++ landing-page/src/components/layout/footer/SocialIcons.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+import twitter from '../../../assets/twitter.svg'
+import discord from '../../../assets/discord.svg'
+import telegram from '../../../assets/whiteTelegram.png'
+
+function SocialIcons() {
+  return (
+    <Ul>
+      <Li><a href={"https://twitter.com/yieldfoundrydao"} target={"_blank"} rel={"noreferrer"}><Img src={twitter} alt={"twitter"} /></a></Li>
+      <Li><a href={"https://discord.gg/defidesk"} target={"_blank"} rel={"noreferrer"}><Img src={discord} alt={"discord"} /></a></Li>
+      <Li><a href={"https://t.me/yfoundrydao"} target={"_blank"} rel={"noreferrer"}><Img src={telegram} alt={"telegram"} /></a></Li>
+    </Ul>
+  )
+}
+
+const Ul = styled.ul`
+  list-style-type:none;
+  width:28%;
+  margin:0 2% 0 0;
+  padding:0;
+  font-weight:600;
+  display:flex;
+  align-items:center;
+  justify-content:space-around;
+  @media(min-width:425px){
+    margin-right:8%;
+    width:25%;
+  }
+  @media(min-width:550px){
+    margin-right:10%;
+    width:22%
+  }
+  @media(min-width:756px){
+    margin-right:12%;
+    width:18%;
+  }
+  @media(min-width:1024px){
+    margin-right:14%;
+    width:16%;
+  }
+`
+
+const Li = styled.li`
+  width:20%;
+  display:flex;
+`
+
+const Img = styled.img`
+  width:100%;
+  margin:0;
+  padding:0;
+`
+
+export default SocialIcons
