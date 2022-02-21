b3a7550ed164e54d7cd917ae58b5f50ec714a939
Trenten <trenten.grede@gmail.com>
2022-02-21 07:35:16 -0600

    added transparent large text behind header

--- landing-page/src/components/sections/roadmap/Roadmap.tsx
+++ landing-page/src/components/sections/roadmap/Roadmap.tsx
@@ -5,6 +5,7 @@ function Roadmap() {
   return (
     <Section>
       <Header>Our <OrangeText>Roadmap</OrangeText></Header>
+      <BackText>Roadmap</BackText>
       <RoadmapTimeline />
     </Section>
   )
@@ -18,7 +19,24 @@ const Section = styled.section`
   margin-top:80px;
 `
 
+const BackText = styled.p`
+  margin: 0;
+  position: relative;
+  font-size:4.5rem;
+  letter-spacing:.02em;
+  font-weight:600;
+  top:-85px;
+  opacity:.2;
+  background:linear-gradient(120deg,rgba(31,67,191,0.6) 0%, rgba(196,188,255,0.3) 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -moz-text-fill-color: transparent;
+  -webkit-text-fill-color: transparent;
+`
+
 const Header = styled.h4`
+  margin: 0;
   text-align:center;
   font-size:1.3rem;
   width:130px;
