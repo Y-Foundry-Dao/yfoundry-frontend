9ba6c28d132d926c124dfcafb236fa9ce73bcaa0
Trenten <trenten.grede@gmail.com>
2022-02-21 23:09:50 -0600

    Refactored code in roadmap component for code quality

--- landing-page/src/components/sections/roadmap/Roadmap.tsx
+++ landing-page/src/components/sections/roadmap/Roadmap.tsx
@@ -9,83 +9,87 @@ function Roadmap() {
       <Header>Our <OrangeText>Roadmap</OrangeText></Header>
       <BackText>Roadmap</BackText>
       <RoadmapSection>
-        <RoadmapBlueBars src={roadmapBlueBars} />
+        <RoadmapBlueBars src={roadmapBlueBars} alt="" role="presentation" />
         <RoadmapTimeline  />
-        <RoadmapOrangeBars src={roadmapOrangeBars} />
+        <RoadmapOrangeBars src={roadmapOrangeBars} alt="" role="presentation" />
       </RoadmapSection>
     </Section>
   )
 }
 
 const Section = styled.section`
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  width:100%;
-  margin-top:80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 80px;
 `
 
 const RoadmapBlueBars = styled.img`
-    width: 10%;
-    margin-top:-650px;
-    margin-right:0;
-    @media(max-width:1170px){
+  width: 10%;
+  margin-top: -650px;
+  margin-right: 0;
+
+  @media(max-width: 1170px){
     display: none;
   }
 `
 const RoadmapOrangeBars = styled.img`
-    width: 10%;
-    margin-top:1450px;
-    @media(max-width:1170px){
+  width: 10%;
+  margin-top: 1450px;
+
+  @media(max-width: 1170px){
     display: none;
   }
 `
 
 const RoadmapSection = styled.div`
   display: flex;
-  justify-content:space-between;
-  width:100%;
+  justify-content: space-between;
+  width: 100%;
 `
 
 const BackText = styled.p`
   margin: 0;
   position: relative;
-  font-size:3.5rem;
-  letter-spacing:.02em;
-  font-weight:600;
-  top:-60px;
-  opacity:.2;
-  background:linear-gradient(120deg,rgba(31,67,191,0.6) 0%, rgba(196,188,255,0.3) 100%);
+  font-size: 3.5rem;
+  letter-spacing: .02em;
+  font-weight: 600;
+  top: -60px;
+  opacity: .2;
+  background: linear-gradient(120deg, rgba(31, 67, 191, 0.6) 0%, rgba(196, 188, 255, 0.3) 100%);
   background-clip: text;
   -webkit-background-clip: text;
   -moz-background-clip: text;
   -moz-text-fill-color: transparent;
   -webkit-text-fill-color: transparent;
-  @media(min-width:756px){
-    font-size:4.5rem;
-    top:-80px
+
+  @media(min-width: 756px){
+    font-size: 4.5rem;
+    top: -80px
   }
 `
 
 const Header = styled.h4`
   margin: 0;
-  text-align:center;
-  font-size:1.3rem;
-  width:130px;
-  @media(min-width:425px){
-    width:140px;
-    font-size:1.5rem;
+  text-align: center;
+  font-size: 1.3rem;
+  width: 130px;
+
+  @media(min-width: 425px){
+    width: 140px;
+    font-size: 1.5rem;
   }
-  @media(min-width:756px){
-    font-size:1.6rem;
+  @media(min-width: 756px){
+    font-size: 1.6rem;
   }
-  @media(min-width:1025px){
-    font-size:1.8rem;
+  @media(min-width: 1025px){
+    font-size: 1.8rem;
   }
 `
 
 const OrangeText = styled.span`
-  color:${props => `${props.theme.colors.orange}`};
+  color: ${props => `${props.theme.colors.orange}`};
 `
 
 export default Roadmap
