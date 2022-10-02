bd6d3a492ec0c9e500ffee2f2e345fac25fd2d34
Ray Raspberry <ray@yfoundry.io>
2022-10-01 23:28:28 -0500

    Roadmap migrated to Progress

--- landing-page/src/components/sections/navigation/NavLinks.tsx
+++ landing-page/src/components/sections/navigation/NavLinks.tsx
@@ -11,7 +11,7 @@ function NavLinks() {
         <li><Link smooth to="community" >join community</Link></li>
         <li><a href="https://app.dework.xyz/y-foundry-dao" target="_blank">bounties</a></li>
         <li><a href="https://brand.yfoundry.io/">brand kit</a></li>
-        <li><Link smooth to="roadmap" >roadmap</Link></li>
+        <li><Link smooth to="roadmap" >progress</Link></li>
       </ul>
     </Nav>
   );
