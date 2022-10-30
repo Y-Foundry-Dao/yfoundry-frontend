a4eb49f7e9ee03ad0bf7c63c5e91dbad7f2deb8a
Ray Raspberry <ray@yfoundry.io>
2022-10-30 13:38:12 -0600

    Updated YFD Script

--- yfd-new-release.sh
+++ yfd-new-release.sh
@@ -4,9 +4,9 @@ git checkout -b releases/v$1 main
 git branch --set-upstream-to=origin/main releases/v$1
 git push
 
-echo "Release v$1 branch prepared for deployment!"
+git tag -a v$1 HEAD -m "New Release v$1"
 
-echo "Go to https://github.com/Y-Foundry-Dao/yfoundry-frontend/releases/new to continue the process."
+echo "Release v$1 branch prepared for deployment!"
 
 echo "Full instructions available at https://docs.yfoundry.io/develop/web2/commit/"
 
